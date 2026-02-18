c9ea4b5c7218d712523bad8abb105440ee23a170
Anushka Singh <anushkasingh50683@gmail.com>
2026-02-18 08:05:53 +0530

    Create table in sqlite3

--- expense_tracker/database.py
+++ expense_tracker/database.py
@@ -4,5 +4,15 @@ conn = sqlite3.connect("expenses.db")
 conn.row_factory = sqlite3.Row
 c = conn.cursor()
 
+c.execute('''
+    Create table if not exists expenses(
+          id integer primary key autoincrement,
+          date text NOT NULL,
+          category text NOT NULL,
+          description text NOT NULL,
+          amount real NOT NULL check (amount > 0)
+          ) 
+''')
+
 conn.commit()
 conn.close()
